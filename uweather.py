478a214c0c40c1f86b325c500917a4306c3d92b4
Demian <ict1986@gmail.com>
2025-05-27 13:30:05 +0200

    Changes for service calling in Appdaemon 4.5+

--- uweather.py
+++ uweather.py
@@ -1,42 +1,18 @@
-import asyncio
 import random
 import json
 from ulanzi import UlanziApp
 
 """
-CAUTION: Due to Appdaemon's limitations, calling the weather.get_forecasts service
-        needs an indirection to get the result of the service call.
-        See https://github.com/AppDaemon/appdaemon/issues/1837
+CAUTION
 
-        This means you need to add a script to homeassistant with the following content:
-```
-alias: get_forecast_now
-mode: single
-icon: mdi:room-service-outline
-fields:
-  call_id:
-    name: Call id
-    default: 1
-    description: An id to uniquely identify the call
-    required: true
-    selector:
-      text: null
-  entity:
-    selector:
-      entity: {}
-    name: entity
-sequence:
-  - target:
-      entity_id: "{{ entity }}"
-    data:
-      type: daily
-    response_variable: response
-    action: weather.get_forecasts
-  - event: call_service_with_response.finished
-    event_data:
-      call_id: "{{ call_id }}"
-      response: "{{ response }}"
-```
+This module is not async, but needs data obtained from the
+weather/get_forecasts service call to Homeassistant.
+
+If something does not work, check if you have the following
+option set in your appdaemon.yaml in the plugins/hass section:
+
+return_result = true
+
 """
 
 ICON_MAP = {
@@ -64,56 +40,31 @@ class UlanziWeather(UlanziApp):
     def initialize(self):
         super().initialize()
         try:
-            self.weather_entity = self.args['weather_entity']
+            self.weather_entity_name = self.args['weather_entity']
+            self.weather_entity = self.get_entity(self.weather_entity_name)
             self.current_temp_sensor = self.args.get('current_temp_sensor')
         except KeyError as err:
             self.error("Failed getting configuration {}".format(err.args[0]))
             return
         
-        self.run_every(self.update_app, 'now', 60)
+        self.update_app()
+        self.run_every(self.update_app, 'now', 300)
         if self.current_temp_sensor:
-            self.listen_state(self.update_app_custom, self.current_temp_sensor)
+            self.listen_state(self.update_app, self.current_temp_sensor)
     
-    def schedule_call_service(self):
-        # call with result, using the script wrapper
-        call_id = random.randrange(2**32)
-
-        self.listen_event(
-            self.real_update_app,
-            "call_service_with_response.finished",
-            call_id=call_id,
-            oneshot=True,
-        )
-
-        self.call_service(
-            "script/get_forecast_now",
-            entity=self.weather_entity,
-            call_id=call_id,
-        )
-
-    def update_app_custom(self, *args, **kwargs):
-        if not self.enabled:
-            return
-
-        # Get forecast for tomorrow, this must be done as a callback
-        forecast = self.schedule_call_service()
-
-    def real_update_app(self, event_name, data, kwargs):
+    def real_update_app(self, forecast_obj):
         # Get current state and temperature
-        current = self.get_state(self.weather_entity, attribute='all')
+        current = self.weather_entity.get_state(attribute="all")
         current_icon = ICON_MAP.get(current['state'], ICON_MAP['exceptional'])
         if self.current_temp_sensor and (t := self.get_state(self.current_temp_sensor)) != 'unknown':
             current_temp = round(float(t), 1)
         else:
             current_temp = round(float(current['attributes']['temperature']), 1)
 
-        # Get forecast for tomorrow
-        forecast_obj = data['response']
-
         if self.now_is_between('00:00:00', '18:00:00'):
-            forecast = forecast_obj[self.weather_entity]['forecast'][0]
+            forecast = forecast_obj['forecast'][0]
         else:
-            forecast = forecast_obj[self.weather_entity]['forecast'][1]
+            forecast = forecast_obj['forecast'][1]
         temp_tomorrow_low = forecast['templow']
         temp_tomorrow_hight = forecast['temperature']
         temp_tomorrow = f"{int(round(temp_tomorrow_low))} - {int(round(temp_tomorrow_hight))}"
@@ -141,7 +92,23 @@ class UlanziWeather(UlanziApp):
         # self.log(f"Updated display: {current_temp} | {temp_tomorrow}")
 
     def update_app(self, *args, **kwargs):
-        self.update_app_custom()
+        if not self.enabled:
+            return
+
+        forecast = self.call_service(
+            "weather/get_forecasts",
+            target={"entity_id": self.weather_entity_name},
+            service_data={"type": "daily"}
+        )
+
+        weather_data = None
+        try:
+            weather_data = forecast['result']['response'][self.weather_entity_name]
+        except KeyError:
+            import pprint
+            self.log("Got unexpected service callback with:" + pprint.pformat(data['result']))
+            return
+        self.real_update_app(weather_data)
 
     def get_app_text(self):
         raise NotImplementedError
